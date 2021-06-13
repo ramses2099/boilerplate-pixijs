cd26a92ea74a57f080d5f0574551397db7152986
ramses2099 <ramses2099@gmail.com>
2021-06-13 11:20:31 -0400

    last commit

--- src/entities.js
+++ src/entities.js
@@ -18,8 +18,51 @@ class Player {
     stage.addChild(this.graphics);
   }
 
+  getBounds() {
+    return this.graphics.getBounds();
+  }
+  
   update(delta) {
     //update
+
+    //draw
+    this.draw();
+  }
+
+  draw() {
+    this.graphics.lineStyle(4, 0xff3300, 1);
+    this.graphics.beginFill(0x66ccff);
+    this.graphics.drawRect(0, 0, this.width, this.height);
+    this.graphics.endFill();
+  }
+}
+
+//
+// NPC Object
+//
+class Npc {
+  constructor(x, y, stage) {
+    this.graphics = new Graphics();
+
+    this.width = 32;
+    this.height = 32;
+    this.graphics.x = x;
+    this.graphics.y = y;
+    this.graphics.vx = 0;
+    this.graphics.vy = 1;
+    this.graphics.interactive = true;
+    this.graphics.alpha = 1;
+    stage.addChild(this.graphics);
+  }
+
+  getBounds() {
+    return this.graphics.getBounds();
+  }
+
+  update(delta) {
+    //update
+
+    this.graphics.y += this.graphics.vy;
 
     //draw
     this.draw();
--- src/init.js
+++ src/init.js
@@ -31,27 +31,34 @@ const app = new Application({
 //add canvas to div
 container.appendChild(app.view);
 
+function isCollition(rect1, rect2){
+    if (rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect1.y > rect2.y) {
+         return true;
+     }
+     return false;
+}
+
+
+
+
 let player;
 let p_width = 32;
 let p_height = 32;
 let p_x = CANVAS_WIDTH / 2 - p_width / 2;
-let p_y = CANVAS_HEIGHT - p_height;
+let p_y = CANVAS_HEIGHT - p_height - 20;
+
+let count_npc = 10;
+let npcArray = [];
+
+let timeSpawn = 100;
+let time = 0;
 
 //setup variables
 function setup() {
-  //   player = new Graphics();
-  //   player.lineStyle(4, 0xff3300, 1);
-  //   player.beginFill(0x66ccff);
-  //   player.drawRect(0, 0, p_width, p_height);
-  //   player.endFill();
-  //   player.x = p_x;
-  //   player.y = p_y;
-  //   player.vx = 0;
-  //   player.vy = 0;
-
   player = new Player(p_x, p_y, p_width, p_height, app.stage);
-
-  //app.stage.addChild(player);
 
   //game loop
   app.ticker.add((delta) => gameLoop(delta));
@@ -59,7 +66,25 @@ function setup() {
 
 //game loop
 function gameLoop(delta) {
-    player.update(delta);
+  player.update(delta);
+  time++;
+
+  if((time % timeSpawn)==0){
+    let x = Math.floor(Math.random() * CANVAS_WIDTH - 32);
+    let y = Math.floor(Math.random() * -32);
+    npcArray.push(new Npc(x, y, app.stage));
+  }
+
+  for (let i = 0; i < npcArray.length; i++) {
+    const npc = npcArray[i];
+
+    if(isCollition(player.getBounds(), npc.getBounds())){
+        console.log('hit');
+    }
+
+
+    npc.update(delta);
+  }
 }
 
 setup();
